7b75735e6346a8b9821bdfd51e23f57f230befe0
ashraful61 <ashraful.ru37@gmail.com>
2024-02-08 17:41:14 +0600

    snap shot testing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>HeadinOne</h1>
-      </header>
+      <p>my test react app</p>
+      
+      {/* <input type='text' placeholder='user name' name="user_name" /> <br /> */}
+      
+      <h1>Snapshot Testing</h1>
+      <p>{data}</p>
+
+      {/* <input type='text' placeholder='full name' name="full_name" onChange={ e => setData(e.target.value)}/> */}
+      <button onClick={ e => setData('Updated data test')}>Update data</button>
+
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
+      <img title='test image title' src="https://api.deepai.org/job-view-file/e67717d4-f9c0-4b32-b1f7-c94e38cf1328/outputs/output.jpg" alt="" />
+   
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,87 @@
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// describe('app component testing', () => {
 
-test('renders learn react heading 1', () => {
+//   test('first react app test case 1', () => {
+//     render(<App />);
+//     const text = screen.getByText("my test react app");
+//     expect(text).toBeInTheDocument();
+//     const imgTitle = screen.getByTitle("test image title");
+//     expect(imgTitle).toBeInTheDocument();
+
+//   });
+
+//   test('input box testing test case 2', () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+
+//     const getPlaceHolderText = screen.getByPlaceholderText("user name");
+//     expect(getPlaceHolderText).toBeInTheDocument();
+
+//     expect(checkInput).toHaveAttribute('name', 'user_name')
+//   });
+
+// })
+
+// describe.skip('app component testing 2', () => {
+
+//   test('description 2', () => {
+//     render(<App />);
+//     const text = screen.queryByText("aasdas as");
+//     expect(text).not.toBeInTheDocument();
+//   });
+
+// })
+
+// describe.only('app component testing 3', () => {
+
+//   test('description 2', () => {
+//     render(<App />);
+//     const text = screen.queryByText("aasdas as");
+//     expect(text).not.toBeInTheDocument();
+//   });
+
+// })
+
+
+
+// describe('app component with Input change event', () => {
+
+//   test('description 2', () => {
+//     render(<App />);
+//     const text = screen.queryByText("aasdas as");
+//     expect(text).not.toBeInTheDocument();
+//   });
+
+// })
+
+// test("On change event testing", () => {
+//   render(<App />);
+
+//   const input = screen.getByRole("textbox");
+
+//   fireEvent.change(input, {target: {value: "a"}})
+//   expect(input.value).toBe('a')
+
+// })
+test("click event testing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/headinone/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  const btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText('Updated data test')).toBeInTheDocument()
+
+})
+
+
+// Snapshot testing 
+
+
+test('snapshot for app component', async () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+
